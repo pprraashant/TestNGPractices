16c1e38b4c41847b316bb4402a4822449d83457e
Prashant <prashant2serve4u@gmail.com>
2018-08-22 11:04:45 +0530

    added new annotations

--- src/test/java/com/test/DriverTest.java
+++ src/test/java/com/test/DriverTest.java
@@ -17,7 +17,8 @@ import org.testng.annotations.AfterSuite;
 
 public class DriverTest {
   @Test(dataProvider = "dp")
-  public void f(Integer n, String s) {
+  public void TC_03_f(Integer n, String s) {
+	  System.out.println("Interger: "+n+" "+"String: "+s);
 	  System.out.println("Inside dataProvider void f method");
   }
   @BeforeMethod
@@ -31,7 +32,7 @@ public class DriverTest {
   }
 
 
-  @DataProvider
+  @DataProvider(name="dp",parallel=true)
   public Object[][] dp() {
 	  System.out.println("Inside dataProvider");
     return new Object[][] {
@@ -39,7 +40,15 @@ public class DriverTest {
       new Object[] { 2, "b" },
     };
   }
-  @BeforeClass
+  @DataProvider(name="dp1")
+  public Object[][] dp1() {
+	  System.out.println("Inside dataProvider");
+    return new Object[][] {
+      new Object[] { 1, "c" },
+      new Object[] { 2, "d" },
+    };
+  }
+  @BeforeClass(alwaysRun=true)
   public void beforeClass() {
 	  System.out.println("BeforeClass");
   }
@@ -70,18 +79,19 @@ public class DriverTest {
   }
 
 
-  @Test
-  public void launchBrowser() {
+  @Test(dataProvider="dp1")
+  public void TC_01_launchBrowser(Integer n, String s) {
 	  
 	  System.out.println("Insider LaunchBrowser Test");
+	  System.out.println("Interger: "+n+" "+"String: "+s);
     Driver driver=new Driver();
     Assert.assertTrue(driver.launchBrowser("chrome"));
     Assert.assertEquals(driver.closeBrowser(), true);
     
   }
   
-  @Test
-  public void closeBrowser() {
+  @Test(alwaysRun=true)
+  public void TC_02_closeBrowser() {
 	  System.out.println("Insider CloseBrowser Test");
     Driver driver=new Driver();
     //Assert.assertTrue(driver.launchBrowser("chrome"));
